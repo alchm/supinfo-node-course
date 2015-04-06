1760f372b8b8dc0a62bc200350145d02bfea0fc8
Baptiste Gouby <baptiste.gouby@gmail.com>
2015-04-06 13:27:26 +0200

    Removed wrong directories

--- TP11-AngularMVC/app.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by baptiste on 2015-03-30.
- */
-var app = angular.module('ProductApp', []);
--- TP11-AngularMVC/models.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by baptiste on 2015-03-30.
- */
-var Product = function(name, description, price) {
-    this.name = name;
-    this.description = description;
-    this.price = price;
-};
